c8a950dc435f0c5c2e61c777d48eb7aa4779b703
Kosta Kiryazov <kosta.kiryazov@gmail.com>
2024-09-11 20:58:49 +0300

    Update teamService.js

--- client/src/services/teamService.js
+++ client/src/services/teamService.js
@@ -1,7 +1,7 @@
 import * as httpClient from '../lib/request';
 
-const baseUrl = 'http://localhost:3030/data/teams';
-//const baseUrl = 'https://travel-agency-server-5epb.onrender.com/data/teams';
+//const baseUrl = 'http://localhost:3030/data/teams';
+const baseUrl = 'https://travel-agency-server-5epb.onrender.com/data/teams';
 
 export const getAll = async () => {
     const packages = await httpClient.get(baseUrl);
